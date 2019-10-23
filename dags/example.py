25a9f1f4071fbb8af7517521a8423e68816ca463
javi <capitan.cambio@gmail.com>
2019-10-23 21:58:38 +0100

    MAke dag use k8s op
    it's the whole point of the talk

--- dags/example.py
+++ dags/example.py
@@ -1,9 +1,8 @@
 import datetime
 
 from airflow import DAG
-# from airflow.contrib.operators.kubernetes_pod_operator import \
-# KubernetesPodOperator
-from airflow.operators import BashOperator
+from airflow.contrib.operators.kubernetes_pod_operator import \
+KubernetesPodOperator
 
 default_args = dict(
     owner="capitancambio",
@@ -17,24 +16,23 @@ default_args = dict(
 )
 
 dag = DAG("example", schedule_interval=None, catchup=False, default_args=default_args)
-task = BashOperator(task_id="my_task", bash_command="echo 'hi, this is python in london'", dag=dag)
 
 
-# task = KubernetesPodOperator(
-# task_id="my_task",
-# name="my-task",
-# namespace="kubernetes-talk",
-# image="scripts:latest",
-# cmds=[
-# "scripts",
-# "transform",
-# "--input-url",
-# "sftp://octopus:tentacle@sftp.kubernetes-talk.svc/upload/input.csv",
-# "--output-url",
-# "sftp://octopus:tentacle@sftp.kubernetes-talk.svc/upload/output.csv",
-# ],
-# get_logs=True,
-# in_cluster=True,
-# image_pull_policy="Never",
-# dag=dag,
-# )
+task = KubernetesPodOperator(
+task_id="my_task",
+name="my-task",
+namespace="kubernetes-talk",
+image="scripts:latest",
+cmds=[
+"scripts",
+"transform",
+"--input-url",
+"sftp://octopus:tentacle@sftp.kubernetes-talk.svc/upload/input.csv",
+"--output-url",
+"sftp://octopus:tentacle@sftp.kubernetes-talk.svc/upload/output.csv",
+],
+get_logs=True,
+in_cluster=True,
+image_pull_policy="Never",
+dag=dag,
+)
